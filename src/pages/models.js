0c9e2ca1f356c5a7a0620dc28653343e41c3579f
John Doe <hamedhasan422421@gmail.com>
2023-10-12 23:35:01 +0300

    🌟 Updated star ratings for vehicle models. 💵 Adjusted pricing information for car rentals. 📝 Made changes to the book button behavior. 📸 Added car images for the vehicle models. 📚 Updated documentation for the Models component. 🖼️ Improved image rendering in the Models section.

---
+++ src/pages/models.js
@@ -0,0 +1,297 @@
+import Footer from "../components/Footer";
+import HeroPages from "../components/HeroPages";
+import CarImage1 from "../images/cars-big/audi-box.png";
+import CarImage2 from "../images/cars-big/golf6-box.png";
+import CarImage3 from "../images/cars-big/benz.jpg";
+import CarImage4 from "../images/cars-big/bmw-box.png";
+import CarImage5 from "../images/cars-big/benz-box.png";
+import CarImage6 from "../images/cars-big/passat-box.png";
+import Link from "next/link";
+import Image from "next/image";
+// import { Link } from "react-router-dom";
+
+function Models() {
+  return (
+    <>
+      <section className="models-section">
+        <HeroPages name="Vehicle Models" />
+        <div className="container">
+          <div className="models-div">
+            <div className="models-div__box">
+              <div className="models-div__box__Image">
+                <Image src={CarImage1} alt="car_Image" />
+                <div className="models-div__box__descr">
+                  <div className="models-div__box__descr__name-price">
+                    <div className="models-div__box__descr__name-price__name">
+                      <p>Audi A1</p>
+                      <span>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                      </span>
+                    </div>
+                    <div className="models-div__box__descr__name-price__price">
+                      <h4>$45</h4>
+                      <p>per day</p>
+                    </div>
+                  </div>
+                  <div className="models-div__box__descr__name-price__details">
+                    <span>
+                      <i className="fa-solid fa-car-side"></i> &nbsp; Audi
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      4/5 &nbsp; <i className="fa-solid fa-car-side"></i>
+                    </span>
+                    <span>
+                      <i className="fa-solid fa-car-side"></i> &nbsp; Manual
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      Diesel &nbsp; <i className="fa-solid fa-car-side"></i>
+                    </span>
+                  </div>
+                  <div className="models-div__box__descr__name-price__btn">
+                    <Link onClick={() => window.scrollhref(0, 0)} href="/">
+                      Book Ride
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="models-div__box">
+              <div className="models-div__box__Image">
+                <Image src={CarImage2} alt="car_Image" />
+                <div className="models-div__box__descr">
+                  <div className="models-div__box__descr__name-price">
+                    <div className="models-div__box__descr__name-price__name">
+                      <p>Golf 6</p>
+                      <span>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                      </span>
+                    </div>
+                    <div className="models-div__box__descr__name-price__price">
+                      <h4>$37</h4>
+                      <p>per day</p>
+                    </div>
+                  </div>
+                  <div className="models-div__box__descr__name-price__details">
+                    <span>
+                      <i className="fa-solid fa-car-side"></i> &nbsp; VW
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      4/5 &nbsp; <i className="fa-solid fa-car-side"></i>
+                    </span>
+                    <span>
+                      <i className="fa-solid fa-car-side"></i> &nbsp; Manual
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      Diesel &nbsp; <i className="fa-solid fa-car-side"></i>
+                    </span>
+                  </div>
+                  <div className="models-div__box__descr__name-price__btn">
+                    <Link onClick={() => window.scrollhref(0, 0)} href="/">
+                      Book Ride
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="models-div__box">
+              <div className="models-div__box__Image">
+                <Image src={CarImage3} alt="car_Image" />
+                <div className="models-div__box__descr">
+                  <div className="models-div__box__descr__name-price">
+                    <div className="models-div__box__descr__name-price__name">
+                      <p>hrefyota</p>
+                      <span>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                      </span>
+                    </div>
+                    <div className="models-div__box__descr__name-price__price">
+                      <h4>$30</h4>
+                      <p>per day</p>
+                    </div>
+                  </div>
+                  <div className="models-div__box__descr__name-price__details">
+                    <span>
+                      <i className="fa-solid fa-car-side"></i> &nbsp; Camry
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      4/5 &nbsp; <i className="fa-solid fa-car-side"></i>
+                    </span>
+                    <span>
+                      <i className="fa-solid fa-car-side"></i> &nbsp; Manual
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      Diesel &nbsp; <i className="fa-solid fa-car-side"></i>
+                    </span>
+                  </div>
+                  <div className="models-div__box__descr__name-price__btn">
+                    <Link onClick={() => window.scrollhref(0, 0)} href="/">
+                      Book Ride
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="models-div__box">
+              <div className="models-div__box__Image">
+                <Image src={CarImage4} alt="car_Image" />
+                <div className="models-div__box__descr">
+                  <div className="models-div__box__descr__name-price">
+                    <div className="models-div__box__descr__name-price__name">
+                      <p>BMW 320</p>
+                      <span>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                      </span>
+                    </div>
+                    <div className="models-div__box__descr__name-price__price">
+                      <h4>$35</h4>
+                      <p>per day</p>
+                    </div>
+                  </div>
+                  <div className="models-div__box__descr__name-price__details">
+                    <span>
+                      <i className="fa-solid fa-car-side"></i> &nbsp; ModernLine
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      4/5 &nbsp; <i className="fa-solid fa-car-side"></i>
+                    </span>
+                    <span>
+                      <i className="fa-solid fa-car-side"></i> &nbsp; Manual
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      Diesel &nbsp; <i className="fa-solid fa-car-side"></i>
+                    </span>
+                  </div>
+                  <div className="models-div__box__descr__name-price__btn">
+                    <Link onClick={() => window.scrollhref(0, 0)} href="/">
+                      Book Ride
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="models-div__box">
+              <div className="models-div__box__Image">
+                <Image src={CarImage5} alt="car_Image" />
+                <div className="models-div__box__descr">
+                  <div className="models-div__box__descr__name-price">
+                    <div className="models-div__box__descr__name-price__name">
+                      <p>Mercedes</p>
+                      <span>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                      </span>
+                    </div>
+                    <div className="models-div__box__descr__name-price__price">
+                      <h4>$50</h4>
+                      <p>per day</p>
+                    </div>
+                  </div>
+                  <div className="models-div__box__descr__name-price__details">
+                    <span>
+                      <i className="fa-solid fa-car-side"></i> &nbsp; Benz GLK
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      4/5 &nbsp; <i className="fa-solid fa-car-side"></i>
+                    </span>
+                    <span>
+                      <i className="fa-solid fa-car-side"></i> &nbsp; Manual
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      Diesel &nbsp; <i className="fa-solid fa-car-side"></i>
+                    </span>
+                  </div>
+                  <div className="models-div__box__descr__name-price__btn">
+                    <Link onClick={() => window.scrollhref(0, 0)} href="/">
+                      Book Ride
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="models-div__box">
+              <div className="models-div__box__Image">
+                <Image src={CarImage6} alt="car_Image" />
+                <div className="models-div__box__descr">
+                  <div className="models-div__box__descr__name-price">
+                    <div className="models-div__box__descr__name-price__name">
+                      <p>VW Passat</p>
+                      <span>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                      </span>
+                    </div>
+                    <div className="models-div__box__descr__name-price__price">
+                      <h4>$25</h4>
+                      <p>per day</p>
+                    </div>
+                  </div>
+                  <div className="models-div__box__descr__name-price__details">
+                    <span>
+                      <i className="fa-solid fa-car-side"></i> &nbsp; CC
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      4/5 &nbsp; <i className="fa-solid fa-car-side"></i>
+                    </span>
+                    <span>
+                      <i className="fa-solid fa-car-side"></i> &nbsp; Manual
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      Diesel &nbsp; <i className="fa-solid fa-car-side"></i>
+                    </span>
+                  </div>
+                  <div className="models-div__box__descr__name-price__btn">
+                    <Link onClick={() => window.scrollhref(0, 0)} href="/">
+                      Book Ride
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="book-banner">
+          <div className="book-banner__overlay"></div>
+          <div className="container">
+            <div className="text-content">
+              <h2>Book a car by getting in hrefuch with us</h2>
+              <span>
+                <i className="fa-solid fa-phone"></i>
+                <h3>(123) 456-7869</h3>
+              </span>
+            </div>
+          </div>
+        </div>
+        <Footer />
+      </section>
+    </>
+  );
+}
+
+export default Models;
