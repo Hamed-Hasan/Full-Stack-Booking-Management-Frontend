8e2ef7eb631afb1214cfec4b58de41f414834a1f
John Doe <hamedhasan422421@gmail.com>
2023-10-13 23:12:18 +0300

    Add icons to navigation items. 🚀 Update icon imports in the data file. 🎨 Refactor navigation data structure. ✨ Implement new icons for the dashboard. 🆕 Update navigation links and icons. 🌐

---
+++ src/dashboard/sidebar/data.tsx
@@ -0,0 +1,39 @@
+import { DocIcon } from "./icons/DocIcon";
+import { HomeIcon } from "./icons/HomeIcon";
+import { StatusIcon } from "./icons/StatusIcon";
+import { CreditIcon } from "./icons/CreditIcon";
+import { ArchiveIcon } from "./icons/ArchiveIcon";
+import { SettingsIcon } from "./icons/SettingsIcon";
+
+export const data = [
+  {
+    title: "Home",
+    icon: <HomeIcon />,
+    link: "/",
+  },
+  {
+    title: "Status",
+    icon: <StatusIcon />,
+    link: "/dashboard/status",
+  },
+  {
+    title: "Archives",
+    icon: <ArchiveIcon />,
+    link: "/dashboard/archives",
+  },
+  {
+    title: "Credits",
+    icon: <CreditIcon />,
+    link: "/dashboard/credits",
+  },
+  {
+    title: "Settings",
+    icon: <SettingsIcon />,
+    link: "/dashboard/settings",
+  },
+  {
+    title: "Documentation",
+    icon: <DocIcon />,
+    link: "/dashboard/documentation",
+  },
+];
