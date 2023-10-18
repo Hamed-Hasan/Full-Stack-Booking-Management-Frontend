a5ff73b210fefeaeab6da9bf471e1ae0f900ce4a
John Doe <hamedhasan422421@gmail.com>
2023-10-18 19:42:53 +0300

    Updated user and superAdmin tags 🔄 Refactored tagTypesList 🌟 Fixed superAdmin type bug 🐞 Added admin tagType functionality 💪 Improved user and admin tags ✨ Renamed superAdmin to superadmin 🔄 Fixed tagType enum for admin 🚚

---
+++ src/redux/tag-types.ts
@@ -0,0 +1,12 @@
+export enum tagTypes {
+    user = "user",
+    admin = "admin",
+    superAdmin="superadmin",
+  }
+  
+  export const tagTypesList = [
+    tagTypes.user,
+    tagTypes.admin,
+    tagTypes.superAdmin,
+  ];
+  
