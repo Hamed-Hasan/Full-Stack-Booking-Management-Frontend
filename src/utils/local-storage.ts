eeb91f0ccc88b22b606254b913c30af8e973e3cc
John Doe <hamedhasan422421@gmail.com>
2023-10-18 19:36:55 +0300

    ✨ Enhance error message extraction for deep property paths 📚 Update function to fetch error messages by property 🛠 Refactor property path error message retrieval

--- src/utils/local-storage.ts
+++ src/utils/local-storage.ts
@@ -2,7 +2,7 @@ export const setToLocalStorage = (key: string, token:string) => {
     if (!key || typeof window === "undefined") {
        return "";
    }
-  return localStorage.setItem(key, token)
+  return localStorage.setItem(key, token);
 };
 
 export const getFromLocalStorage = (key: string) => {
