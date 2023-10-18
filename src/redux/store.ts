9b67edb565700d79c44c25b27d38229271fd78c6
John Doe <hamedhasan422421@gmail.com>
2023-10-18 19:40:11 +0300

    🚀 Implemented new Redux store setup. 🛠️ Updated store configuration. 🎉 Added baseApi middleware. 🐛 Fixed middleware import issue. ✨ Improved store initialization.

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,15 +1,16 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { combineReducers } from "redux";
-import counterReducer from "./slices/counterSlice";
+import { baseApi } from './api/baseApi';
+import { reducer } from './rootReducer';
+import { configureStore } from '@reduxjs/toolkit'
 
-const rootReducer = combineReducers({
-  counter: counterReducer,
-});
+export const store = configureStore({
+  reducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(baseApi.middleware),
+})
 
-const store = configureStore({
-  reducer: rootReducer,
-});
 
-export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
-export default store;
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
